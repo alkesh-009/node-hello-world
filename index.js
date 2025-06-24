bb16f9f866309aed30e9d35baf76a393b01c17df
root <root@ip-10-0-15-78.eu-central-1.compute.internal>
2025-06-24 17:14:31 +0000

    Dockerfile and index.js ready

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1 style="color: red;">Hello from Node.js deployed via CI/CD</h1>');
+});
+
+app.listen(PORT, () => console.log(`App running on ${PORT}`));
+
